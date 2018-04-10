1289ed5640217c92d0f372c00fe509d0ad1bfdfe
Nick D'Angelo <sjasper313@gmail.com>
2018-04-10 02:16:51 +0000

    anothercommit

---
+++ Lesson7_MyRecipe/myrecipe.js
@@ -0,0 +1,20 @@
+/*
+Program Name: Recipe Display Application
+Author:Nicholas D'Angelo
+Date:04/06/2018
+Filename: script.js
+*/
+/* global $ */
+//displays the next element after the current target
+function display(event) {
+    $(event.currentTarget).next().fadeIn("slow");
+}//end of display
+function hide(event) {
+    $(event.currentTarget).next().fadeOut("slow");
+}//end of display
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3").click(display);
+$("h1").after("<center>Enjoy!!</center>");
+
+
+
